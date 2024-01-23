abf513e1fcf14cc55e088a1b3052aa93e55ff7a7
test <test@gmail.com>
2024-01-23 10:29:13 +0900

    개인프로젝트-수정

--- javascript-study/추가-개인프로젝트/index.js
+++ javascript-study/추가-개인프로젝트/index.js
@@ -1,26 +1,39 @@
 const chatSreenContent = document.getElementById('chat-scroll');
 const chatTextContent = document.getElementById('chat-screen');
 const chatForm = document.getElementById('chat-form');
+const deleteAlert = document.getElementById('delete-alert');
 
-let myChatArr = [];
 let yourChatArr = [];
+let AllChatArr = [];
 
-function ChatContent(chatText, chatId) {
+function ChatContent(chatText, chatId, chatWho) {
     this.chatText = chatText;
     this.chatId = chatId;
+    this.chatWho = chatWho;
 }
 
-// 로컬에 디폴트 채팅내역 저장
+// 디폴트 채팅내역 저장
 let chatContents = document.querySelectorAll('.text');
 chatContents.forEach((el) => {
-    const toBeAdded = new ChatContent(el.innerHTML, new Date().getTime());
+    const toBeAdded = new ChatContent(el.innerHTML, new Date().getTime(), "you");
     yourChatArr.push(toBeAdded);
-    saveChats();
 })
+
+function displayDefaltChat() {
+    for (let i = 0; i < 2; i++) {
+        const AllChat = document.createElement('li');
+
+        AllChat.innerHTML = yourChatArr[i].chatText;
+        AllChat.classList.add("text", "left");
+
+        chatTextContent.appendChild(AllChat);
+    }
+}
+displayDefaltChat();
 
 // 로컬 저장소에 저장하기
 function saveChats() {
-    const chatString = JSON.stringify(yourChatArr);
+    const chatString = JSON.stringify(AllChatArr);
     localStorage.setItem("Chats", chatString);
 }
 
@@ -28,98 +41,109 @@ function saveChats() {
 function loadChats() {
     const Chats = localStorage.getItem("Chats");
     if (Chats !== null) {
-        yourChatArr = JSON.parse(Chats);
+        AllChatArr = JSON.parse(Chats);
+        displayAllChat();
     }
 }
 loadChats();
 
+// 메세지 삭제
 function handleChatDbClick(clickedId) {
-    if (confirm("메시지를 삭제하시겠습니까?")) {
-        myChatArr = myChatArr.filter(function (aChat) {
+    deleteAlert.style.display = "block";
+
+    const confirmBtn = deleteAlert.children[1];
+    const cancelBtn = deleteAlert.children[2];
+
+    function deleteMsg() {
+        AllChatArr = AllChatArr.filter(function (aChat) {
             return aChat.chatId !== clickedId;
         })
-    }
-    displayMyChat();
-}
-
-function displayDefaltChat() {
-    for (let i = 0; i < 2; i++) {
-        const YourChat = document.createElement('li');
-
-        YourChat.innerHTML = yourChatArr[i].chatText;
-        YourChat.classList.add("text", "left");
-
-        chatTextContent.appendChild(YourChat);
-    }
-}
-
-function displayYourChat(chatText) {
-    const YourChat = document.createElement('li');
-    const chatTime = document.createElement('span');
-
-    if (chatText.includes("수업")) {
-        YourChat.textContent = yourChatArr[2].chatText;
-    } else if (chatText.includes("추천")) {
-        YourChat.textContent = yourChatArr[3].chatText;
-    } else if (chatText.includes("고마워")) {
-        YourChat.textContent = yourChatArr[4].chatText;
-    } else if (chatText.includes("슈붕")) {
-        YourChat.textContent = yourChatArr[5].chatText;
+        displayAllChat()
+        saveChats();
+        deleteAlert.style.display = "none";
     }
 
-    YourChat.classList.add("text", "left");
-
-    let options = { hour: "numeric", minute: "numeric" };
-    const now = new Date();
-    chatTime.textContent = now.toLocaleTimeString("ko-KR", options);
-
-    YourChat.appendChild(chatTime);
-    chatTextContent.appendChild(YourChat);
-
-    chatSreenContent.scrollTo(0, chatSreenContent.scrollHeight);
+    function cancelDelete() {
+        deleteAlert.style.display = "none";
+    }
+    confirmBtn.onclick = deleteMsg;
+    cancelBtn.onclick = cancelDelete;
+    // 삭제시 스크롤 위치 그대로 유지
+    history.scrollRestoration = "auto"
 }
 
-
-function displayMyChat() {
+// 채팅 보여주기
+function displayAllChat() {
     chatTextContent.innerHTML = "";
     displayDefaltChat();
-    myChatArr.forEach(function (aChat) {
-        const MyChat = document.createElement('li');
+    AllChatArr.forEach(function (aChat) {
+        const AllChat = document.createElement('li');
         const chatTime = document.createElement('span');
 
-        MyChat.textContent = aChat.chatText;
+        AllChat.innerHTML = aChat.chatText;
 
-        MyChat.classList.add("text", "right");
+        if (aChat.chatWho === "me") {
+            AllChat.classList.add("text", "right");
+        } else if (aChat.chatWho === "you") {
+            AllChat.classList.add("text", "left");
+        }
+        AllChat.title = "더블클릭하여 삭제";
 
         let options = { hour: "numeric", minute: "numeric" };
         const now = new Date();
         chatTime.textContent = now.toLocaleTimeString("ko-KR", options);
 
-        MyChat.addEventListener("dblclick", function () {
+        AllChat.addEventListener("dblclick", function () {
             handleChatDbClick(aChat.chatId);
         })
 
-        MyChat.appendChild(chatTime);
-        chatTextContent.appendChild(MyChat);
-
-        if (aChat.chatText.includes("수업") || aChat.chatText.includes("추천") || aChat.chatText.includes("고마워") || aChat.chatText.includes("슈붕")) {
-            displayYourChat(aChat.chatText);
-        }
-        chatSreenContent.scrollTo(0, chatSreenContent.scrollHeight);
+        AllChat.appendChild(chatTime);
+        chatTextContent.appendChild(AllChat);
     })
+}
+
+// 상대방의 메세지 설정
+function settingYourChat(chatText) {
+
+    if (chatText.includes("수업")) {
+        if (!AllChatArr.includes(yourChatArr[2])) {
+            AllChatArr.push(yourChatArr[2]);
+        }
+    } else if (chatText.includes("추천")) {
+        if (!AllChatArr.includes(yourChatArr[3])) {
+            AllChatArr.push(yourChatArr[3]);
+        }
+    } else if (chatText.includes("고마워")) {
+        if (!AllChatArr.includes(yourChatArr[4])) {
+            AllChatArr.push(yourChatArr[4]);
+        }
+    } else if (chatText.includes("슈붕")) {
+        if (!AllChatArr.includes(yourChatArr[5])) {
+            AllChatArr.push(yourChatArr[5]);
+        }
+    }
+}
+
+// 나의 메세지
+function settingMyChat(aChat) {
+    AllChatArr.push(aChat);
+    displayAllChat();
+
+    settingYourChat(aChat.chatText);
+
+    saveChats();
+    setTimeout(displayAllChat, 3000);
+    // 메시지 추가시 스크롤 맨 밑으로
+    chatSreenContent.scrollTop = chatSreenContent.scrollHeight;
 }
 
 chatForm.addEventListener("submit", function (e) {
     e.preventDefault();
-    // const toBeAdded = {
-    //     chatText: chatForm.chatText.value,
-    //     chatId: new Date().getTime(),
-    //     chatWho: "me"
-    // };
-    const toBeAdded = new ChatContent(chatForm.chatText.value, new Date().getTime());
-    chatForm.chatText.value = "";
-    myChatArr.push(toBeAdded);
 
-    displayMyChat();
-    saveChats();
+    if (chatForm.chatText.value != "") {
+        const toBeAdded = new ChatContent(chatForm.chatText.value, new Date().getTime(), "me");
+        chatForm.chatText.value = "";
+        settingMyChat(toBeAdded);
+        //setTimeout(displayAllChat, 3000);
+    }
 })
