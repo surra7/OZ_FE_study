1567eb648c8e0dd1727e6d24fe3579e1260be897
surra7 <subum6302@gmail.com>
2024-01-25 22:12:25 +0900

    Fix: request 이벤트 중복 처리되는 오류 removeEventListener로 해결

--- javascript-study/Challenge/Ch22-강아지정보페이지/Ch22-강아지정보페이지.js
+++ javascript-study/Challenge/Ch22-강아지정보페이지/Ch22-강아지정보페이지.js
@@ -3,112 +3,103 @@ const apiAllBreeds = "https://dog.ceo/api/breeds/list/all";
 const request1 = new XMLHttpRequest();
 const request2 = new XMLHttpRequest();
 
-const header = document.getElementById("header");
-const main = document.getElementById("main");
-const input = document.getElementById("filter-text");
-const button = document.getElementById("filter-button");
-const select = document.getElementById("filter-select");
-const resetBtn = document.getElementById('reset');
-const more = document.getElementById('more');
-const tothetop = document.getElementById('tothetop');
+const header = document.getElementById("header")
+const main = document.getElementById("main")
+const input = document.getElementById("filter-text")
+const button = document.getElementById("filter-button")
+const select = document.getElementById("filter-select")
+const reset = document.getElementById('reset');
+const more = document.getElementById("more")
+const tothetop = document.getElementById("tothetop")
 
-let currentDogs = [];
+let currentDogs = []
 
-// 사진 뿌리는 함수
 function displayDogs(item) {
-  const dogImgDiv = document.createElement("div");
-  dogImgDiv.classList.add("flex-item");
+  const dogImgDiv = document.createElement("div")
+  dogImgDiv.classList.add("flex-item")
   dogImgDiv.innerHTML = `
-            <img src=${item}>
-        `;
-  main.appendChild(dogImgDiv);
+    <img src=${item}>
+  `
+  main.appendChild(dogImgDiv)
 }
 
-// requst1 load함수
-function request1handleLoad() {
-  const response = JSON.parse(request1.response);
-  response.message.forEach(function (item) {
-    currentDogs.push(item);
-    displayDogs(item);
-  });
-}
-
-//웹 페이지가 처음 로딩되었을 때
+// 페이지가 새로 로딩 되었을 때
 window.addEventListener("load", function () {
   // 강아지 사진 뿌리기
-  request1.open("get", apiRandomDogs);
-  request1.addEventListener("load", request1handleLoad);
-  request1.send();
+  request1.open("GET", apiRandomDogs)
+  request1.addEventListener("load", handleRequest1)
+  request1.send()
 
-  // 셀렉트에 견종 정보 뿌리기
-  request2.open("get", apiAllBreeds);
+  // 견종 텍스트 뿌리기
+  request2.open("GET", apiAllBreeds)
   request2.addEventListener("load", function () {
-    const response = JSON.parse(request2.response);
+    const response = JSON.parse(request2.response)
     Object.keys(response.message).forEach(function (item) {
-      const option = document.createElement("option");
-      option.textContent = item;
-      option.value = item;
-      select.appendChild(option);
+      const option = document.createElement("option")
+      option.textContent = item
+      option.value = item
+      select.appendChild(option)
     });
-  });
-  request2.send();
-});
+  })
+  request2.send()
+})
 
-// 견종 입력했을 때 필터링
+// 필터링 단어 검색
 button.addEventListener("click", function () {
-  main.innerHTML = "";
-  // currentDogs에 있는 이미지 src에는 견종 이름이 포함되어 있음 그 이름을 가지고 필터링
+  main.innerHTML = ""
   let filteredDogs = currentDogs.filter(function (item) {
-    return item.indexOf(input.value) !== -1;
-  });
-
-  input.value = "";
-
+    return item.indexOf(input.value) !== -1
+  })
+  input.value = ""
   filteredDogs.forEach(function (item) {
-    displayDogs(item);
+    displayDogs(item)
   });
-});
+})
 
-// 셀랙트에서 골랐을 때 필터링
+// 셀랙트에서 필터링
 select.addEventListener("change", function () {
-  main.innerHTML = "";
+  main.innerHTML = ""
   let filteredDogs = currentDogs.filter(function (item) {
-    return item.indexOf(select.value) !== -1;
-  });
+    return item.indexOf(select.value) !== -1
+  })
 
   filteredDogs.forEach(function (item) {
-    displayDogs(item);
+    displayDogs(item)
   });
-});
+})
 
-// 리셋 버튼 눌렀을 때 새로운 강아지 사진 뿌리기
-resetBtn.addEventListener('click', function () {
+// 다시 강아지 사진 42개 가져오기
+reset.addEventListener('click', function () {
   main.innerHTML = "";
   select.value = "";
   currentDogs = [];
 
-  request1.removeEventListener('load', request1handleLoad);
-  request1.open("get", apiRandomDogs);
-  request1.addEventListener("load", request1handleLoad);
-  request1.send();
+  request1.removeEventListener('load', handleRequest1)
+  request1.open("GET", apiRandomDogs)
+  request1.addEventListener("load", handleRequest1)
+  request1.send()
 })
 
-// more 눌렀을 때 강아지 사진 더 가져오기
-more.addEventListener('click', function () {
-  request1.open('get', apiRandomDogs);
-  request1.addEventListener('load', function () {
-    const response = JSON.parse(request1.response);
-    response.message.forEach(function (item) {
-      currentDogs.push(item);
-      displayDogs(item);
-    });
-  })
-  request1.send();
+// 강아지 사진 42개 더 가져오기
+more.addEventListener("click", function () {
+  select.value = "";
+  // removeEvent를 해줘야 이전 사진 가져오는 이벤트가 취소되서 한번만 추가가 됨!!!
+  request1.removeEventListener('load', handleRequest1)
+  request1.open("GET", apiRandomDogs)
+  request1.addEventListener("load", handleRequest1)
+  request1.send()
 })
 
-// top 눌렀을때 스크롤 맨 위로
-tothetop.addEventListener('click', function () {
-  // scrollTo: 주어진 위치로 스크롤을 이동
-  // 위치에 대한 값을 객체 리터럴 형태로 줌
-  window.scrollTo({ top: 0 });
+// 스크롤 맨 위로
+tothetop.addEventListener("click", function () {
+  window.scrollTo({ top: 0 })
 })
+
+// request1에 대한 이벤트 함수
+function handleRequest1() {
+  const response = JSON.parse(request1.response)
+  response.message.forEach(function (item) {
+    currentDogs.push(item)
+    displayDogs(item)
+  });
+}
